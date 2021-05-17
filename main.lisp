88472c5ae2837b1b3a914ed8f5ab4ec6a24c9b15
Iavael <iavael@users.noreply.github.com>
2021-05-17 20:41:07 +0300

    Some cleanup

--- main.lisp
+++ main.lisp
@@ -1,21 +1,16 @@
 (load "analyse.lisp")
 (load "calculate.lisp")
-(load "synthesyse.lisp")
+(load "synthesize.lisp")
 
 (defun main ()
   (parentheses-off
     (calculate-expr
       (analyse-expr nil '(nil)
         '(
-;;          (2 * x + 5 - 4 * x) ^ 2 - (3 + 6 - 2 + 19) ^ 3 = ( 4 * x + 1 ) * x + x ^ (5 - 2 ^ 2)
            (x + (x + 1)) + (2 + (x + 3)) - (x + (x - 1)) - (2 + (x - 3)) +
            (x + (1 + x)) + (2 + (3 + x)) - (x + (1 - x)) - (2 + (3 - x)) +
            ((x + 1) + x) + ((x + 3) + 2) - ((x - 1) + x) - ((x - 3) + 2) +
            ((1 + x) + x) + ((3 + x) + 2) - ((1 - x) + x) - ((3 - x) + 2)
-;           (x * (x * 4)) * (2 * (x * 3)) / (x * (x / 4)) / (2 * (x / 3)) *
-;           (x * (4 * x)) * (2 * (3 * x)) / (x * (4 / x)) / (2 * (3 / x)) *
-;           ((x * 4) * x) * ((x * 3) * 2) / ((x / 4) * x) / ((x / 3) * 2) *
-;           ((4 * x) * x) * ((3 * x) * 2) / ((4 / x) * x) / ((3 / x) * 2)
            )
         )
       )
